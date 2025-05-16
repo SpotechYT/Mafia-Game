eacc5a2064941c8bf68c8eccfa3e6e92a5f67a48
Nikhil Appu <nik.appu@gmail.com>
2025-05-16 11:28:31 -0700

    made basic constructor, getters, and setters for the Player class

--- Mafia/src/Player.java
+++ Mafia/src/Player.java
@@ -2,4 +2,59 @@ public class Player {
     /*TODO:
     * Variables: name, role, alive, votes, PathToIcon
     */
+    private String name;
+    private String role;
+    private boolean alive;
+    private int votes;
+    private String pathToIcon;
+
+    public Player(String name, int votes, String pathToIcon) {
+        this.name = name;
+        this.role = "civilian"; // Default to civilian
+        this.alive = true; // Default to alive
+        this.votes = votes;
+        this.pathToIcon = pathToIcon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public String getPathToIcon() {
+        return pathToIcon;
+    }
+
+    public void setPathToIcon(String pathToIcon) {
+        this.pathToIcon = pathToIcon;
+    }
+
+    
 }
