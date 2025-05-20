c7c19d0e0714a66ede9482ea9c1ab16e6007d7f9
Nikhil Appu <nik.appu@gmail.com>
2025-05-20 11:41:42 -0700

    updating settings

--- Mafia/src/SettingPanel.java
+++ Mafia/src/SettingPanel.java
@@ -1,11 +1,11 @@
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
+
 
 public class SettingPanel extends JPanel {
 
     public JButton backButton;
+    public JButton nameButton;
 
     public SettingPanel() {
         // Set layout
@@ -20,7 +20,17 @@ public class SettingPanel extends JPanel {
 
         // Add top panel to the top (NORTH) of the main panel
         add(topPanel, BorderLayout.NORTH);
-
         // You can add game content to other parts of the panel later
+        JPanel centerPanel = new JPanel(new GridLayout(1, 2));
+
+        JPanel leftPanel = new JPanel(new BorderLayout());
+        nameButton = new JButton("Change Name");
+        leftPanel.setBorder(BorderFactory.createTitledBorder("Settings"));
+        leftPanel.add(new JScrollPane(nameButton), BorderLayout.NORTH);
+
+        centerPanel.add(leftPanel);
+
+        add(centerPanel, BorderLayout.CENTER);
+        
     }
 }
