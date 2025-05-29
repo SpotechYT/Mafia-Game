ef2746ba822e2dd83c9588ee24e1c100c7914df5
AsB0507 <asbuchm@gmail.com>
2025-05-29 10:35:37 -0700

    gui update

--- Mafia/src/MainMenu.java
+++ Mafia/src/MainMenu.java
@@ -1,8 +1,10 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 
 public class MainMenu extends JPanel {
 
@@ -14,15 +16,30 @@ public class MainMenu extends JPanel {
 
         JLabel label = new JLabel("The Mafia Game", JLabel.CENTER);
         add(label, BorderLayout.NORTH);
+        
 
         JPanel buttonPanel = new JPanel(new GridLayout(1, 2, 10, 10));
-        playButton = new JButton("Play");
-        settingsButton = new JButton("Settings");
+        playButton = new JButton();
+        
+       
+
+        ImageIcon icon = new ImageIcon("Graphics/playButton.png");
+        playButton.setIcon(icon);
+        playButton.setBackground(Color.black);
+        playButton.setBorder(null);
+        
+        
+        ImageIcon settingsIcon = new ImageIcon("Graphics/settings.png");
+        settingsButton = new JButton(settingsIcon);
+        settingsButton.setBackground(Color.black);
+        settingsButton.setBorder(null);
 
         buttonPanel.add(playButton);
         buttonPanel.add(settingsButton);
 
         add(buttonPanel, BorderLayout.CENTER);
+        setBackground(Color.darkGray);
+        validate();
     }
 
 }
