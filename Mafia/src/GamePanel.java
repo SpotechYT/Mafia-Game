842d8bb7142776385e4321d26c0b1dc29f24d3c3
Haniel Mikhaiel <hanielmikhaiel@spotech.dev>
2025-06-04 11:25:38 -0700

    Bug Fixes

--- Mafia/src/GamePanel.java
+++ Mafia/src/GamePanel.java
@@ -158,7 +158,7 @@ public class GamePanel extends JPanel {
                         // If in choose save mode, send a save request for the player
                         game.contactAllPlayers("SAVE:" + player);
                         break;
-                    case "CHOSE_VICTIM":
+                    case "CHOOSE_VICTIM":
                         // If in choose victim mode, send a victim request for the player
                         game.contactAllPlayers("VICTIM:" + player);
                         break;
